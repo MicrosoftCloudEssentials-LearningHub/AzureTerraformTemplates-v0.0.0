7faec6c9cbf5e6466fde40b5069b15273f966a39
brown9804 <timnab@microsoft.com>
2025-03-19 16:01:02 -0600

    mariadb is not supported

--- 1_storage-databases/mariadb/outputs.tf
+++
@@ -1,8 +0,0 @@
-# File for defining output values
-output "mariadb_server_name" {
-  value = azapi_resource.mariadb_server.name
-}
-
-output "mariadb_database_name" {
-  value = azapi_resource.mariadb_database.name
-}
